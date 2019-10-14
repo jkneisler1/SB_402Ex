e1c0847af878d0a1270c84ec9230be8745b0d397
jkneisler1 <John.Kneisler@gmail.com>
2019-10-14 14:39:35 -0400

    First submission

---
+++ src/main/java/com/example/demo/Sb402ExApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sb402ExApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sb402ExApplication.class, args);
+    }
+
+}
